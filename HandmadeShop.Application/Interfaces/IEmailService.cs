ad5480c6ba194cd25edb87ad278657dd9cfb5fa3
tamnguyen2005 <your-email@example.com>
2026-04-04 17:19:36 +0700

    feat:Add send otp feature

---
+++ HandmadeShop.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace HandmadeShop.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+    }
+}
---
+++ HandmadeShop.Infrastructure/Provider/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using HandmadeShop.Application.Interfaces;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace HandmadeShop.Infrastructure.Provider
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config["EmailSettings:SenderEmail"]));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            var builder = new BodyBuilder { HtmlBody = body };
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            try
+            {
+                await smtp.ConnectAsync(_config["EmailSettings:Host"], int.Parse(_config["EmailSettings:Port"]), SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(_config["EmailSettings:SenderEmail"], _config["EmailSettings:Password"]);
+                await smtp.SendAsync(email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi gửi mail: {ex.Message}");
+            }
+            finally
+            {
+                await smtp.DisconnectAsync(true);
+            }
+        }
+    }
+}
