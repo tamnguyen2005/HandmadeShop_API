6faad374788e07afecc44a2ddac9c7c4653a3eae
tamnguyen2005 <your-email@example.com>
2026-04-04 17:27:02 +0700

    Update: cascade for user

---
+++ HandmadeShop.Infrastructure/Migrations/20260404081652_DestroyUser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandmadeShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DestroyUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_User_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
